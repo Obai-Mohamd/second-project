2f49913441a170ee6dcb11d39e2e7c005b402611
Obai-Mohamd <103772357+Obai-Mohamd@users.noreply.github.com>
2023-10-08 20:37:52 +0300

    Add files via upload

---
+++ src/assets/nav.jsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+export default function Nav() {
+    return (
+        <nav>
+            <img src="./assets/airbnb.svg" className="nav--logo" />
+        </nav>
+    )
+}
